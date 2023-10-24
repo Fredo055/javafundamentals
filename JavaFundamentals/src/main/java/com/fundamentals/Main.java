9eae4517da3beb7240a157fe3ad5be11f190d46a
Alfredo Jurado <fredointech5@gmail.com>
2023-10-24 17:42:16 -0500

    My Application Overview

--- JavaFundamentals/src/main/java/com/fundamentals/Main.java
+++ JavaFundamentals/src/main/java/com/fundamentals/Main.java
@@ -2,6 +2,38 @@ package com.fundamentals;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        // System.out.println("Hello Java!");
+        // houseExample();
+        // houseReference();
     }
+
+    // This is a single comment
+    /*
+    * This a multi-line comment
+    * */
+
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.foundationType = "Concrete";
+        myHouse.doorColor = "Red";
+        myHouse.roofStyle = "Shingle";
+        myHouse.windowSize = 42;
+        myHouse.doorFunction();
+        System.out.println(myHouse.doorColor);
+    }
+
+        public static void houseReference() {
+            House myHouse = new House();
+            House otherHouse = new House();
+            House yourHouse = otherHouse;
+
+            myHouse.doorColor = "Red";
+            otherHouse.doorColor = "Blue";
+            System.out.println(myHouse.doorColor);
+            System.out.println(otherHouse.doorColor);
+            System.out.println(yourHouse.doorColor);
+            yourHouse.doorColor = "Green";
+            System.out.println(yourHouse.doorColor);
+            System.out.println(otherHouse.doorColor);
+        }
 }
