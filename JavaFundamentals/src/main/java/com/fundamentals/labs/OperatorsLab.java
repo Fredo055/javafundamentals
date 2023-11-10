c89d5c799ee47b806c05e2fa349dc9d2dc7053ab
Alfredo Jurado <fredointech5@gmail.com>
2023-11-09 20:33:07 -0600

    OperatorsLab
    3 tasks completed

--- JavaFundamentals/src/main/java/com/fundamentals/labs/OperatorsLab.java
+++ JavaFundamentals/src/main/java/com/fundamentals/labs/OperatorsLab.java
@@ -1,2 +1,38 @@
-package com.fundamentals.labs;public class OperatorsLab {
+package com.fundamentals.labs;
+
+import java.util.Scanner;
+
+public class OperatorsLab {
+
+    public void demo1 () {
+        double total = (10 + 32) * 12/3;
+        int two = 10 + 32 * 12/3;
+        System.out.println(two);
+        System.out.println(total);
+    } // end of method
+
+    public void demoTwo(int bob, int bravo) {
+        bob += bravo;
+        System.out.println("= operator" + bob);
+        bob *= bravo;
+        System.out.println("*= operator" + bob);
+        bob %= bravo;
+        System.out.println("%= operator" + bob);
+    } // end of method
+
+
+    public void Scanner() {
+        int localVarible = 5, num1, total;
+        System.out.println("Enter integers");
+        Scanner userInput = new Scanner(System.in);
+        num1 = userInput.nextInt();
+        total = num1 % localVarible;
+        total = num1 % localVarible;
+        System.out.println(" User value % " + total);
+
+
+    }  // end method
+
+
+
 }
